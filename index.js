38f945c4b525e7f25e2248968d0309b603828991
Rahul Batra <97456632+BtraRahul@users.noreply.github.com>
2023-08-01 02:17:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function setAndReturnWinner() {
   var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
   var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-  var source1 = "/images/dice" + randomNumber1 + ".png"; // /images/dice1.png to /images/dice6.png
-  var source2 = "/images/dice" + randomNumber2 + ".png";
+  var source1 = "images/dice" + randomNumber1 + ".png"; // /images/dice1.png to /images/dice6.png
+  var source2 = "images/dice" + randomNumber2 + ".png";
 
   document.querySelectorAll("img")[0].setAttribute("src", source1);
   document.querySelectorAll("img")[1].setAttribute("src", source2);
